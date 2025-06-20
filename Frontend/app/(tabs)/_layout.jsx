cb0051d655d7ac7a515abfd6eacbc2b665628aa5
A. S. M. Muhit Khan <muhitkhan.taosif@gmail.com>
2025-06-21 04:50:34 +0600

    Dashboard Implemented with routes to child

--- Frontend/app/(tabs)/_layout.jsx
+++ Frontend/app/(tabs)/_layout.jsx
@@ -16,7 +16,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="(dashboard)"
         options={{
           title: "Dashboard",
           tabBarIcon: ({ color, size = 24 }) => (
